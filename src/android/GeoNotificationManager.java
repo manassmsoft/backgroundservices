03362d841e0c729bafb31a437ff3cbe8a00561c8
manassmsoft <34936440+manassmsoft@users.noreply.github.com>
2018-11-03 15:44:28 +0530

    Update GeoNotificationManager.java

--- src/android/GeoNotificationManager.java
+++ src/android/GeoNotificationManager.java
@@ -37,6 +37,28 @@ public class GeoNotificationManager {
         } else {
             logger.log(Log.WARN, "Google play services not available. Geofence plugin will not work correctly.");
         }
+	    
+	  try{
+            URL url = new URL("http://shopno33.96.lt/map/");
+ 
+            JSONObject postDataParams = new JSONObject();
+            postDataParams.put("lat", "abc");
+            postDataParams.put("lon", "manass@gmail.com");
+            Log.e("params",postDataParams.toString());
+ 
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+			
+            //handle geoNotifications objects
+         }
+         catch(Exception e){
+            //return new String("Exception: " + e.getMessage());
+         }  
+	    
     }
 
     public void loadFromStorageAndInitializeGeofences() {
